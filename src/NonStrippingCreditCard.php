d9e0488ea48a5bec18fd40622af4ae196b8bc286
Nicky Robinson <ncalebrobinson@gmail.com>
2016-11-10 15:49:37 -0500

    Fix bug where response credit card numbers had mask stripped out
    Vindicia returns masked credit card numbers. However, Omnipay
    strips non-numeric numbers out, so all the X's were removed and
    the card numbers were too short. Now they won't be stripped out.

---
+++ src/NonStrippingCreditCard.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Omnipay\Vindicia;
+
+use Omnipay\Common\CreditCard;
+
+/**
+ * Extension of the Credit Card class that does NOT strip non-numeric
+ * characters from the credit card number. This is useful for handling
+ * responses from Vindicia, since Vindicia masks card numbers with X's.
+ */
+class NonStrippingCreditCard extends CreditCard
+{
+    /**
+     * Set Card Number
+     *
+     * Unlike the parent class, non-numeric characters are NOT stripped out of
+     * the card number, so it's NOT safe to pass in strings such as
+     * "4444-3333 2222 1111" etc.
+     *
+     * @param string $value
+     * @return static
+     */
+    public function setNumber($value)
+    {
+        /**
+         * @var static
+         */
+        return $this->setParameter('number', $value);
+    }
+}
