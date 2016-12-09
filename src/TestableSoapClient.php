0f76685fec5dfcb7777f38bfdbeb40fedcb206ce
Nicky Robinson <ncalebrobinson@gmail.com>
2016-12-09 11:07:07 -0500

    Remove hack for bug in psalm that has been fixed

--- src/TestableSoapClient.php
+++ src/TestableSoapClient.php
@@ -34,10 +34,6 @@ class TestableSoapClient extends SoapClient
     protected static $lastFunctionName;
     protected static $lastArguments;
 
-    /**
-     * @psalm-suppress InaccessibleMethod SoapClient doesn't have __construct. PHP knows to look for SoapClient,
-     * Psalm does not.
-     */
     public function __construct($wsdl, array $options = null)
     {
         if (!isset(self::$nextResponseOverrideQueue)) {
