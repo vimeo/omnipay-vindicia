2c8d6216dc74e2255f698303de4624e35c92da67
rondogency <dd208@sina.com>
2018-06-07 14:11:21 -0400

    add page to fetch transactions

--- src/Message/FetchTransactionsRequest.php
+++ src/Message/FetchTransactionsRequest.php
@@ -67,6 +67,8 @@ use Omnipay\Common\Exception\InvalidRequestException;
  */
 class FetchTransactionsRequest extends AbstractRequest
 {
+    const DEFAULT_PAGE_SIZE = 10000;
+
     /**
      * The name of the function to be called in Vindicia's API
      *
@@ -83,6 +85,52 @@ class FetchTransactionsRequest extends AbstractRequest
     protected function getObject()
     {
         return self::$TRANSACTION_OBJECT;
+    }
+
+    /**
+     * Get the number of records to return per call
+     *
+     * @return null|int
+     */
+    public function getPageSize()
+    {
+        return $this->getParameter('pageSize');
+    }
+
+    /**
+     * Set the number of records to return per call
+     *
+     * @param int $value
+     * @return static
+     */
+    public function setPageSize($value)
+    {
+        return $this->setParameter('pageSize', $value);
+    }
+
+    /**
+     * Get the page to return. Starts at 0.
+     * For example, if pageSize is 10 and page is 0, returns the first 10
+     * results. If page is 1, returns the second 10 results.
+     *
+     * @return null|int
+     */
+    public function getPage()
+    {
+        return $this->getParameter('page');
+    }
+
+    /**
+     * Set the page to return. Starts at 0.
+     * For example, if pageSize is 10 and page is 0, returns the first 10
+     * results. If page is 1, returns the second 10 results.
+     *
+     * @param int $value
+     * @return static
+     */
+    public function setPage($value)
+    {
+        return $this->setParameter('page', $value);
     }
 
     public function getData()
@@ -115,6 +163,8 @@ class FetchTransactionsRequest extends AbstractRequest
         } else {
             $data['timestamp'] = $startTime;
             $data['endTimestamp'] = $endTime;
+            $data['page'] = $this->getPage() ?: 0;
+            $data['pageSize'] = $this->getPageSize() ?: self::DEFAULT_PAGE_SIZE;
         }
 
         return $data;
